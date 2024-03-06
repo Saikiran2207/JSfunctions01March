3f97e94924f71de7ffbf5694942e27e1eac2a969
Saikiran2207 <81770915+Saikiran2207@users.noreply.github.com>
2024-03-06 11:18:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function change(){
+    document.getElementById("changeborder1").style.border="solid 5px green";
+    //document.getElementById("changeborder2").style.border="solid 5px red";
+}
+function change1(){
+    //document.getElementById("changeborder1").style.border="solid 5px red";
+    document.getElementById("changeborder2").style.border="dotted 5px black";
+}
+function change2(){
+    //document.getElementById("changeborder1").style.border="solid 5px red";
+    document.getElementById("changeborder3").style.border="dashed 5px red";
+}
+function change3(){
+    //document.getElementById("changeborder1").style.border="solid 5px red";
+    document.getElementById("changeborder4").style.border="double 5px brown";
+}
+function change4(){
+    //document.getElementById("changeborder1").style.border="solid 5px red";
+    document.getElementById("changeborder5").style.border="groove 5px violet";
+}
